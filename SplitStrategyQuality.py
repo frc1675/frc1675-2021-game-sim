1772b87beb5b046a22ae7376e6dd76a0a0494962
AaronMcGarry <aaronmcgarry42@gmail.com>
2021-02-19 16:33:08 -0600

    Added average and bad robot quality

--- SplitStrategyQuality.py
+++ SplitStrategyQuality.py
@@ -257,6 +257,98 @@ ROBOT_QUALITY = {
             "Cycle_StdDev": 1.3,
             "Reliability": 92
         }
+    },
+    "AVERAGE": {
+        "Low Painting": {
+            "Cycle": 15,
+            "Cycle_StdDev": 1.2,
+            "Auto Reliability": 75,
+            "Reliability": 83
+        },
+        "Mid Painting": {
+            "Cycle": 17,
+            "Cycle_StdDev": 1.4,
+            "Auto Reliability": 72,
+            "Reliability": 80
+        },
+        "High Painting": {
+            "Cycle": 19,
+            "Cycle_StdDev": 1.6,
+            "Auto Reliability": 69,
+            "Reliability": 77
+        },
+        "Floor Painting": {
+            "Cycle": 15,
+            "Cycle_StdDev": 2.2,
+            "Reliability": 90
+        },
+        "Near Statue": {
+            "Cycle": 25,
+            "Cycle_StdDev": 3,
+            "Auto Reliability": 92,
+            "Reliability": 95
+        },
+        "Far Statue": {
+            "Cycle": 35,
+            "Cycle_StdDev": 5,
+            "Reliability": 95
+        },
+        "Fire System": {
+            "Cycle": 3,
+            "Cycle_StdDev": 0.4,
+            "Reliability": 98
+        },
+        "Chain Pull": {
+            "Cycle": 12,
+            "Cycle_StdDev": 1.3,
+            "Reliability": 80
+        }
+    },
+    "BAD": {
+        "Low Painting": {
+            "Cycle": 20,
+            "Cycle_StdDev": 1.2,
+            "Auto Reliability": 70,
+            "Reliability": 75
+        },
+        "Mid Painting": {
+            "Cycle": 24,
+            "Cycle_StdDev": 1.4,
+            "Auto Reliability": 65,
+            "Reliability": 72
+        },
+        "High Painting": {
+            "Cycle": 28,
+            "Cycle_StdDev": 1.6,
+            "Auto Reliability": 60,
+            "Reliability": 69
+        },
+        "Floor Painting": {
+            "Cycle": 15,
+            "Cycle_StdDev": 2.2,
+            "Reliability": 80
+        },
+        "Near Statue": {
+            "Cycle": 32,
+            "Cycle_StdDev": 3,
+            "Auto Reliability": 75,
+            "Reliability": 80
+        },
+        "Far Statue": {
+            "Cycle": 40,
+            "Cycle_StdDev": 5,
+            "Reliability": 80
+        },
+        "Fire System": {
+            "Cycle": 5,
+            "Cycle_StdDev": 0.4,
+            "Reliability": 98
+        },
+        "Chain Pull": {
+            "Cycle": 15,
+            "Cycle_StdDev": 1.3,
+            "Reliability": 50
+        }
     }
 }
 
