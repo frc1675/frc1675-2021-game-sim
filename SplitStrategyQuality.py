a05875c26333c115639b8139ba1163188e7e3592
AaronMcGarry <aaronmcgarry42@gmail.com>
2021-02-19 16:24:33 -0600

    Added new robot types from the survey

--- SplitStrategyQuality.py
+++ SplitStrategyQuality.py
@@ -33,6 +33,181 @@ ROBOT_STRATEGY = {
         "Chain Pull": {
             "Endgame Priority": 1
         }
+    },
+    "PAINTINGS ONLY": {
+        "Low Painting": {
+            "Auto Priority": 3,
+            "TeleOp Priority": 3,
+            "Endgame Priority": 4
+        },
+        "Mid Painting": {
+            "Auto Priority": 2,
+            "TeleOp Priority": 2,
+            "Endgame Priority": 3
+        },
+        "High Painting": {
+            "Auto Priority": 1,
+            "TeleOp Priority": 1,
+            "Endgame Priority": 2
+        },
+        "Floor Painting": {
+            "TeleOp Priority": 5,
+            "Endgame Priority": 6
+        },
+        "Near Statue": {
+            "Auto Priority": 10,
+            "TeleOp Priority": 10
+        },
+        "Far Statue": {
+            "TeleOp Priority": 10
+        },
+        "Fire System": {
+            "TeleOp Priority": 4,
+            "Endgame Priority": 5
+        },
+        "Chain Pull": {
+            "Endgame Priority": 1
+        }
+    },
+    "STATUES ONLY": {
+        "Low Painting": {
+            "Auto Priority": 10,
+            "TeleOp Priority": 10,
+            "Endgame Priority": 10
+        },
+        "Mid Painting": {
+            "Auto Priority": 10,
+            "TeleOp Priority": 10,
+            "Endgame Priority": 10
+        },
+        "High Painting": {
+            "Auto Priority": 10,
+            "TeleOp Priority": 10,
+            "Endgame Priority": 10
+        },
+        "Floor Painting": {
+            "TeleOp Priority": 10,
+            "Endgame Priority": 10
+        },
+        "Near Statue": {
+            "Auto Priority": 1,
+            "TeleOp Priority": 2
+        },
+        "Far Statue": {
+            "TeleOp Priority": 3
+        },
+        "Fire System": {
+            "TeleOp Priority": 1,
+            "Endgame Priority": 2
+        },
+        "Chain Pull": {
+            "Endgame Priority": 1
+        }
+    },
+    "STUATUES+LOW PAINTINGS": {
+        "Low Painting": {
+            "Auto Priority": 1,
+            "TeleOp Priority": 1,
+            "Endgame Priority": 2
+        },
+        "Mid Painting": {
+            "Auto Priority": 10,
+            "TeleOp Priority": 10,
+            "Endgame Priority": 10
+        },
+        "High Painting": {
+            "Auto Priority": 10,
+            "TeleOp Priority": 10,
+            "Endgame Priority": 10
+        },
+        "Floor Painting": {
+            "TeleOp Priority": 2,
+            "Endgame Priority": 4
+        },
+        "Near Statue": {
+            "Auto Priority": 2,
+            "TeleOp Priority": 2
+        },
+        "Far Statue": {
+            "TeleOp Priority": 3
+        },
+        "Fire System": {
+            "TeleOp Priority": 4,
+            "Endgame Priority": 3
+        },
+        "Chain Pull": {
+            "Endgame Priority": 1
+        }
+    },
+    "LOW PAINTINGS ONLY": {
+        "Low Painting": {
+            "Auto Priority": 1,
+            "TeleOp Priority": 1,
+            "Endgame Priority": 2
+        },
+        "Mid Painting": {
+            "Auto Priority": 10,
+            "TeleOp Priority": 10,
+            "Endgame Priority": 10
+        },
+        "High Painting": {
+            "Auto Priority": 10,
+            "TeleOp Priority": 10,
+            "Endgame Priority": 10
+        },
+        "Floor Painting": {
+            "TeleOp Priority": 2,
+            "Endgame Priority": 4
+        },
+        "Near Statue": {
+            "Auto Priority": 10,
+            "TeleOp Priority": 10
+        },
+        "Far Statue": {
+            "TeleOp Priority": 10
+        },
+        "Fire System": {
+            "TeleOp Priority": 3,
+            "Endgame Priority": 3
+        },
+        "Chain Pull": {
+            "Endgame Priority": 1
+        }
+    },
+    "CHASSIS BOT": {
+        "Low Painting": {
+            "Auto Priority": 10,
+            "TeleOp Priority": 10,
+            "Endgame Priority": 10
+        },
+        "Mid Painting": {
+            "Auto Priority": 10,
+            "TeleOp Priority": 10,
+            "Endgame Priority": 10
+        },
+        "High Painting": {
+            "Auto Priority": 10,
+            "TeleOp Priority": 10,
+            "Endgame Priority": 10
+        },
+        "Floor Painting": {
+            "TeleOp Priority": 10,
+            "Endgame Priority": 10
+        },
+        "Near Statue": {
+            "Auto Priority": 10,
+            "TeleOp Priority": 10
+        },
+        "Far Statue": {
+            "TeleOp Priority": 2
+        },
+        "Fire System": {
+            "TeleOp Priority": 1,
+            "Endgame Priority": 2
+        },
+        "Chain Pull": {
+            "Endgame Priority": 1
+        }
     }
 }
 
